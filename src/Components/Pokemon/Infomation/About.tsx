21b25a2ca4e20e8a4abc0561e34d4a1eb23661c5
Hassan-Ib <azanebrahim@gmail.com>
2022-06-16 14:35:31 -0600

    fixing pokemon page

--- src/Components/Pokemon/Infomation/About.tsx
+++ src/Components/Pokemon/Infomation/About.tsx
@@ -18,7 +18,7 @@ const About = (props: Props) => {
   console.log(height, weight, abilities, species);
   return (
     <div>
-      <section className="flex gap-4 text-sm justify-center bg-[hsl(329,100%,5%)] border border-red-400 rounded-xl font-semibold py-3 w-fit mx-auto px-4">
+      <section className=" flex-col sm:flex-row flex gap-4 text-sm justify-center items-center bg-[hsl(329,100%,5%)] border border-red-400 rounded-xl font-semibold py-3 w-fit mx-auto px-4 my-6">
         <div>
           <div className="flex items-center gap-2">
             <GiWeightScale className="text-red-400" />{" "}
@@ -27,7 +27,7 @@ const About = (props: Props) => {
           </div>
           <div className=" capitalize opacity-75 text-center mt-1">height</div>
         </div>
-        <div>_</div>
+        <div className="text-ceneter">_</div>
         <div>
           <div className="flex items-center gap-2">
             <FaRulerVertical className="text-red-400" />{" "}
@@ -36,6 +36,18 @@ const About = (props: Props) => {
           </div>
           <div className=" capitalize opacity-75 text-center mt-1">weight</div>
         </div>
+      </section>
+      <section>
+        <h2 className="my-4 font-semibold px-1">Abilites</h2>
+        <ul className="flex gap-2">
+          {abilities.map((ability: { ability: { name: string } }) => (
+            <li
+              key={ability.ability.name}
+              className="capitalize px-6   py-1 bg-[hsl(329,100%,5%)] opacity-80 border border-red-400 rounded-2xl text-sm ">
+              <span>{ability.ability.name}</span>
+            </li>
+          ))}
+        </ul>
       </section>
     </div>
   );
--- src/Components/Pokemon/Infomation/InformationLayout.tsx
+++ src/Components/Pokemon/Infomation/InformationLayout.tsx
@@ -4,7 +4,7 @@ import Navigation from "./Navigation";
 
 export const InformationLayout = () => {
   return (
-    <div className="flex flex-col text-white border border-red-500 p-6 rounded-2xl">
+    <div className="flex flex-col text-white border border-red-500 p-6 rounded-2xl overflow-hidden">
       <Navigation />
       <Outlet />
     </div>
